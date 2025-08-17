fb3db329392ec34cc47dd04734988332b50e336b
ishchukserhii <ischuksergei@gmail.com>
2025-08-17 20:05:51 +0300

    1

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    if (!data) {
+      return [];
+    }
+    return JSON.parse(data);
+  } catch (err) {
+    console.error('Помилка читання файлу:', err);
+    return [];
+  }
+};
